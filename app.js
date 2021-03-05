28f1c39d7aa36d5e2d331ac1fa2d4464dc1cb3c8
nithisha-javadi <68708842+nithisha-javadi@users.noreply.github.com>
2021-03-05 13:06:31 -0500

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ var app = angular.module('TransformersApp', []);
 app.controller('BattleController', function($scope, BattleRulesService, TransformerDecoratorFactory) {
 
   $scope.transformersInput;
-
+  
+  // Setting the loosing team criteria
   $scope.setLoosingTeam = function() {
     if ($scope.autobotVictories - $scope.decepticonVictories !== 0) {
       $scope.loosingTeam = $scope.autobotVictories > $scope.decepticonVictories ? "D" : "A";
@@ -15,7 +16,8 @@ app.controller('BattleController', function($scope, BattleRulesService, Transfor
   $scope.setRemainingBots = function() {
     $scope.remainingBots = angular.equals($scope.loosingTeam, "D") ? $scope.decepticons : $scope.autobots;
   };
-
+  
+  // Setting the winner team criteria
   $scope.setLastWinner = function(winnerBots, loosingTeam) {
     var winners = _.remove(winnerBots, function(transformer) {
       return !angular.equals(transformer.team, loosingTeam);
@@ -23,7 +25,8 @@ app.controller('BattleController', function($scope, BattleRulesService, Transfor
 
     $scope.lastWinner = winners[winners.length - 1];
   };
-
+  
+  // Calculating the team points
   $scope.increaseTeamPoints = function(winner) {
     if (angular.equals(winner.team, "D")) {
       $scope.decepticonVictories++;
@@ -31,7 +34,8 @@ app.controller('BattleController', function($scope, BattleRulesService, Transfor
       $scope.autobotVictories++;
     }
   };
-
+ 
+  // Resetting the Arena
   $scope.resetArena = function() {
     $scope.battleLineup = [];
     $scope.autobots = [];
@@ -47,13 +51,17 @@ app.controller('BattleController', function($scope, BattleRulesService, Transfor
   };
 
   $scope.prepareTeams = function(allTransformers) {
+    
+    // listing all the autobots tranformers in ascending order by their ranks
     $scope.autobots = _.sortBy(allTransformers, 'rank');
 
+    // listing all the deception transformers
     $scope.decepticons = _.remove($scope.autobots, function(transformer) {
       return angular.equals(transformer.team, "D");
     });
   };
 
+  // Function for checking if the teams have fighters
   $scope.checkBothTeamsHaveFighters = function() {
     return $scope.autobots.length > 0 && $scope.decepticons.length > 0;
   };
@@ -73,7 +81,8 @@ app.controller('BattleController', function($scope, BattleRulesService, Transfor
         break;
     }
   };
-
+ 
+  // Function for resolving the battles criteria
   $scope.resolveBattles = function() {
 
     $scope.resetArena();
